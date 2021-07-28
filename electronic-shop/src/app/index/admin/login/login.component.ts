425aa4b9843f6bb8a28f210d6e0a8c244166ed60
dianogabriel <78863187+dianogabriel@users.noreply.github.com>
2021-07-28 20:08:34 +0800

    fefef
    fefef

--- electronic-shop/src/app/index/admin/login/login.component.ts
+++ electronic-shop/src/app/index/admin/login/login.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+ selector: 'app-login',
+ templateUrl: './login.component.html',
+ styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds: DataService, public route: Router) { }
+
+  userInfo: any = {};
+  account: any = {};
+  user_name: any;
+  user_password: any;
 
   ngOnInit(): void {
   }
+
+  //PASSWORD HIDE 
+  hide = true;
+
+  async regUser() {
+
+    this.userInfo.user_name = "Administrator";
+    this.userInfo.user_password = "12345";
+
+    await this.ds.sendApiRequest("regUser", this.userInfo).subscribe((res: any) => {
+      console.log(res);
+    })
+  }
+
+  async loginUser() {
+    this.userInfo.user_name = this.user_name;
+    this.userInfo.user_password = this.user_password;
+
+    await this.ds.sendApiRequest("loginUser", this.userInfo).subscribe((res: any) => {
+      console.log(res);
+
+      if (res.payload.length == 0) {
+        alert("Incorrect Credentials");
+      }
+      else {
+        localStorage.setItem("Name", res.payload.user_name);
+        this.route.navigate(['/home']);
+      }
+    });
+  }
+
 
 }
